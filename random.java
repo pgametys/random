66cd2128a363b61ea9c3d0758ad656223c2fe7e9
pgametys <50024591+pgametys@users.noreply.github.com>
2019-06-19 16:47:25 +0200

    Update random.java

--- random.java
+++ random.java
@@ -1,1 +1,13 @@
-//Random randomness
+public class Random
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(getRand());
+    }
+
+    public int getRand()
+    {
+      return 4;
+    }
+}
